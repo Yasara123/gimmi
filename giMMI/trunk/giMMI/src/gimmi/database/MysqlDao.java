8d0adbb124675579421b3f8c1be14d3ec4aaf6ea
penn-narr@web.de <penn-narr@web.de@855ebb53-4e8f-d8f2-0007-f2798c5b8a64>
2011-12-21 06:45:36 +0000

    * Add basic tests for logging & database - Setup is working now

--- giMMI/trunk/giMMI/src/gimmi/database/MysqlDao.java
+++ giMMI/trunk/giMMI/src/gimmi/database/MysqlDao.java
@@ -7,103 +7,107 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class MysqlDao {
-	private Connection connection = null;
-	private Statement statement = null;
-	private ResultSet resultSet = null;
-	private static MysqlDao instance = null;
+    private Connection connection = null;
+    private Statement statement = null;
+    private ResultSet resultSet = null;
+    private static MysqlDao instance = null;
 
-	private String database = "opinion-crawling";
-	private String user = "root";
-	private String password = "";
+    // TODO: get this through a config-manager
+    private final String database = "gimmi";
+    private final String user = "root";
+    private final String password = "";
 
-	/**
-	 * 
-	 * @throws SQLException
-	 */
-	private MysqlDao() throws SQLException {
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			connection = DriverManager
-					.getConnection("jdbc:mysql://localhost/" + database + "?"
-							+ "user=" + user + "&password=" + password);
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+    /**
+     * 
+     * @throws SQLException
+     */
+    private MysqlDao() throws SQLException {
+	try {
+	    Class.forName("com.mysql.jdbc.Driver");
+	    this.connection = DriverManager
+		    .getConnection("jdbc:mysql://localhost/" + this.database
+			    + "?" + "user=" + this.user + "&password="
+			    + this.password);
+	} catch (ClassNotFoundException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
 	}
+    }
 
-	/**
-	 * Gibt Instanz zurueck
-	 * 
-	 * @return MysqlDao
-	 */
-	public static MysqlDao getInstance() {
-		if (instance == null) {
-			try {
-				instance = new MysqlDao();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		return instance;
+    /**
+     * Gibt Instanz zurueck
+     * 
+     * @return MysqlDao
+     */
+    public static MysqlDao getInstance() {
+	if (instance == null) {
+	    try {
+		instance = new MysqlDao();
+	    } catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	    }
 	}
-	
-	public Connection getConnection() {
-		return connection;
+	return instance;
+    }
+
+    public Connection getConnection() {
+	return this.connection;
+    }
+
+    public ResultSet getResultset(String query) {
+	try {
+	    this.statement = this.connection.createStatement();
+	    this.resultSet = this.statement.executeQuery(query);
+	} catch (Exception e) {
+	    e.printStackTrace();
 	}
+	return this.resultSet;
+    }
 
-	public ResultSet getResultset(String query) {
-		try {
-			statement = connection.createStatement();
-			resultSet = statement.executeQuery(query);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return resultSet;
+    /**
+     * Speichert einen Datensatz in einer spezifizierten Tabelle und gibt die
+     * erzeugte auto_increment ID zurueck Bei einem nicht erfolgreichen
+     * Speichern, bzw wenn kein auto_increment Wert erzeugt wurde wurde wird -1
+     * zurueckgegeben
+     * 
+     * @param table
+     * @param values
+     * @return int
+     */
+    public int insert(String table, Row values) {
+	StringBuffer cols = new StringBuffer();
+	StringBuffer vals = new StringBuffer();
+	String[] value = new String[2];
+
+	for (int i = 0; i < (values.getNumOfCols() - 1); i++) {
+	    value = values.next();
+	    cols.append("`" + value[0] + "`, ");
+	    vals.append("'" + value[1] + "', ");
 	}
+	value = values.next();
+	cols.append("`" + value[0] + "`");
+	vals.append("'" + value[1] + "'");
 
-	/**
-	 * Speichert einen Datensatz in einer spezifizierten Tabelle und gibt die erzeugte
-	 * auto_increment ID zurueck
-	 * Bei einem nicht erfolgreichen Speichern, bzw wenn kein auto_increment Wert erzeugt wurde
-	 * wurde wird -1 zurueckgegeben
-	 * @param table
-	 * @param values
-	 * @return int
-	 */
-	public int insert(String table, Row values) {
-		StringBuffer cols = new StringBuffer();
-		StringBuffer vals = new StringBuffer();
-		String[] value = new String[2];
+	String insert = "INSERT INTO `" + table + "`" + "(" + cols.toString()
+		+ ")" + "VALUES" + "(" + vals.toString() + ");";
+	System.out.println(insert);
 
-		for (int i = 0; i < (values.getNumOfCols() - 1); i++) {
-			value = values.next();
-			cols.append("`" + value[0] + "`, ");
-			vals.append("'" + value[1] + "', ");
-		}
-		value = values.next();
-		cols.append("`" + value[0] + "`");
-		vals.append("'" + value[1] + "'");
-
-		String insert = "INSERT INTO `" + table + "`" + "(" + cols.toString()
-				+ ")" + "VALUES" + "(" + vals.toString() + ");";
-		System.out.println(insert);
-		
-		int autoIncKeyFromApi = -1;
-		try {
-			Statement st = connection.createStatement();
-			st.executeUpdate(insert, Statement.RETURN_GENERATED_KEYS);
-			ResultSet rs = st.getGeneratedKeys();
-			if (rs.next()) {
-		        autoIncKeyFromApi = rs.getInt(1);
-		    } else {
-		    	throw new SQLException("Es konnt kein Auto Increment Wert gefunden werden!");
-		    	//kreative fehlerbehandlung
-		    }
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return autoIncKeyFromApi;
+	int autoIncKeyFromApi = -1;
+	try {
+	    Statement st = this.connection.createStatement();
+	    st.executeUpdate(insert, Statement.RETURN_GENERATED_KEYS);
+	    ResultSet rs = st.getGeneratedKeys();
+	    if (rs.next()) {
+		autoIncKeyFromApi = rs.getInt(1);
+	    } else {
+		throw new SQLException(
+			"Es konnt kein Auto Increment Wert gefunden werden!");
+		// kreative fehlerbehandlung
+	    }
+	} catch (SQLException e) {
+	    e.printStackTrace();
 	}
+	return autoIncKeyFromApi;
+    }
 }
