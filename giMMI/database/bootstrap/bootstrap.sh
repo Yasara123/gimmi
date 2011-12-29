fb0cfe9ff546bf2d4bfcb6263edc3c074e37d4ca
penn-narr@web.de <penn-narr@web.de@855ebb53-4e8f-d8f2-0007-f2798c5b8a64>
2011-12-29 12:38:41 +0000

    corrected file naming

--- giMMI/database/bootstrap/bootstrap.sh
+++ giMMI/database/bootstrap/bootstrap.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 # insert some static data into the DB
 mysqlimport --host=127.0.0.1 --port=3306 --user=root --columns=country_code,name_en --fields-terminated-by=";" --fields-enclosed-by='"' --lines-terminated-by="\n" --delete -p gimmi --local --ignore-lines=2 --verbose=TRUE country.csv
