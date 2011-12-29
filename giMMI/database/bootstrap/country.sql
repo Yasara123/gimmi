3029dc201bb6decc815e117094a2f0f1fb2d2a9d
penn-narr <penn-narr@web.de>
2011-12-29 02:23:26 +0000

    - add language codes table data - updated db-model (utf8 collations and missing auto increments)

--- giMMI/database/bootstrap/country.sql
+++ giMMI/database/bootstrap/country.sql
@@ -1,12 +1,28 @@
 SET character_set_server=utf8;
 SET collation_server=utf8_bin;
 
+-- -------------------------------------
+-- COUNTRY CODES
+-- -------------------------------------
 DELETE FROM gimmi.country;
 
-LOAD DATA LOCAL INFILE 'ISO3166-1_alpha-2.csv' REPLACE
+LOAD DATA LOCAL INFILE '/home/bbz/workspace/giMMI database/bootstrap/ISO3166-1_alpha-2.csv' REPLACE
 INTO TABLE gimmi.country
 CHARACTER SET utf8
 FIELDS TERMINATED BY';'
 LINES TERMINATED BY '\n'
 IGNORE 2 LINES
 (country_code, name_en);
+
+-- -------------------------------------
+-- LANGUAGE CODES
+-- -------------------------------------
+DELETE FROM gimmi.language;
+
+LOAD DATA LOCAL INFILE '/home/bbz/workspace/giMMI database/bootstrap/ISO639-alpha-3.csv' REPLACE
+INTO TABLE gimmi.language
+CHARACTER SET utf8
+FIELDS TERMINATED BY'|'
+LINES TERMINATED BY '\n'
+IGNORE 2 LINES
+(lang_code, @ignore, @ignore, name_en);
