f7d91f2cbe33e7e6288ca0dded8d5a36e4f00869
penn-narr <penn-narr@web.de>
2012-01-01 22:52:33 +0000

    Site API working (needs database revision 32+) - moved api-client to new example package

--- giMMI/trunk/giMMI/src/gimmi/database/Database.java
+++ giMMI/trunk/giMMI/src/gimmi/database/Database.java
@@ -5,24 +5,30 @@ import gimmi.util.ConfigManagerException;
 
 import java.sql.SQLException;
 
+/**
+ * Databasee adapter
+ * 
+ * @author Jens Bertram <code@jens-bertram.net>
+ * 
+ */
 public class Database {
-	static CorpusDatabase database = null;
+    static CorpusDatabase database = null;
 
-	private static CorpusDatabase initDatabase() throws SQLException,
-			ConfigManagerException {
-		if (ConfigManager.isInitialized() == false) {
-			ConfigManager.tryLoadFallbackConfig();
-		}
-		Database.database = new gimmi.database.mysql.Database(
-				ConfigManager.getByKey("database.name"),
-				ConfigManager.getByKey("database.user"),
-				ConfigManager.getByKey("database.password"));
-		return Database.database;
+    private static CorpusDatabase initDatabase() throws SQLException,
+	    ConfigManagerException {
+	if (ConfigManager.isInitialized() == false) {
+	    ConfigManager.tryLoadFallbackConfig();
 	}
+	Database.database = new gimmi.database.mysql.Database(
+		ConfigManager.getByKey("database.name"),
+		ConfigManager.getByKey("database.user"),
+		ConfigManager.getByKey("database.password"));
+	return Database.database;
+    }
 
-	static public CorpusDatabase getDatabase() throws SQLException,
-			ConfigManagerException {
-		return (Database.database == null) ? Database.initDatabase()
-				: Database.database;
-	}
+    static public CorpusDatabase getDatabase() throws SQLException,
+	    ConfigManagerException {
+	return (Database.database == null) ? Database.initDatabase()
+		: Database.database;
+    }
 }
