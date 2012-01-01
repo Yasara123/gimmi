a0c79d1e45c1cbd8f5b123b96397b1915ace6289
penn-narr@web.de <penn-narr@web.de@855ebb53-4e8f-d8f2-0007-f2798c5b8a64>
2012-01-01 22:37:03 +0000

    Site API working

--- giMMI/trunk/giMMI/src/gimmi/content/CorpusContent.java
+++ giMMI/trunk/giMMI/src/gimmi/content/CorpusContent.java
@@ -13,83 +13,83 @@ import java.util.HashMap;
  * 
  */
 public abstract class CorpusContent {
-	protected CorpusDatabaseTable table;
-	protected HashMap<String, Object> rowData;
+    protected CorpusDatabaseTable table;
+    protected HashMap<String, Object> rowData;
 
-	/**
-	 * Set the database table associated with this CorpusContent
-	 * 
-	 * @param table
-	 *            The database table
-	 * @return The database table given as parameter
-	 */
-	protected void setTable(CorpusDatabaseTable table) {
-		this.table = table;
-		this.rowData = new HashMap<String, Object>(this.getTable().getColumns()
-				.size());
-	}
+    /**
+     * Set the database table associated with this CorpusContent
+     * 
+     * @param table
+     *            The database table
+     * @return The database table given as parameter
+     */
+    protected void setTable(CorpusDatabaseTable table) {
+	this.table = table;
+	this.rowData = new HashMap<String, Object>(this.getTable().getColumns()
+		.size());
+    }
 
-	/**
-	 * Get the database table associated with this CorpusContent
-	 * 
-	 * @return The database table associated with this CorpusContent
-	 */
-	public CorpusDatabaseTable getTable() {
-		return this.table;
-	}
+    /**
+     * Get the database table associated with this CorpusContent
+     * 
+     * @return The database table associated with this CorpusContent
+     */
+    public CorpusDatabaseTable getTable() {
+	return this.table;
+    }
 
-	/**
-	 * Check if a field exists in the current table
-	 * 
-	 * @param name
-	 *            The field name to check for
-	 * @throws CorpusDatabaseException
-	 *             Thrown if field is not valid
-	 */
-	protected void checkField(String name) throws CorpusDatabaseException {
-		if (this.getTable().getColumns().containsKey(name) != true) {
-			throw new CorpusDatabaseException(
-					CorpusDatabaseException.Error.COLUMN_NOT_FOUND.toString(),
-					name);
-		}
+    /**
+     * Check if a field exists in the current table
+     * 
+     * @param name
+     *            The field name to check for
+     * @throws CorpusDatabaseException
+     *             Thrown if field is not valid
+     */
+    protected void checkField(String name) throws CorpusDatabaseException {
+	if (this.getTable().getColumns().containsKey(name) != true) {
+	    throw new CorpusDatabaseException(
+		    CorpusDatabaseException.Error.COLUMN_NOT_FOUND,
+		    this.getTable() + "." + name);
 	}
+    }
 
-	/**
-	 * Set object field data for writing to the database. Implementing objects
-	 * must overwrite this function to implement a storing of this data.
-	 * 
-	 * @param name
-	 *            The name of the property to set
-	 * @param data
-	 *            The content of the property
-	 * @throws CorpusDatabaseException
-	 *             Thrown if the property does not exist in the database
-	 */
-	public void setProperty(String name, Object data)
-			throws CorpusDatabaseException {
-		// check if field is valid
-		this.checkField(name);
-		this.rowData.put(name, data);
-	}
+    /**
+     * Set object field data for writing to the database. Implementing objects
+     * must overwrite this function to implement a storing of this data.
+     * 
+     * @param name
+     *            The name of the property to set
+     * @param data
+     *            The content of the property
+     * @throws CorpusDatabaseException
+     *             Thrown if the property does not exist in the database
+     */
+    public void setProperty(String name, Object data)
+	    throws CorpusDatabaseException {
+	// check if field is valid
+	this.checkField(name);
+	this.rowData.put(name, data);
+    }
 
-	/**
-	 * Set object field data for writing to the database.
-	 * 
-	 * @param data
-	 *            A Hashmap with property key-value pairs
-	 * @throws CorpusDatabaseException
-	 *             Thrown if a property does not exist in the database
-	 */
-	public void setProperties(HashMap<String, Object> data)
-			throws CorpusDatabaseException {
-		// check if fields are valid
-		for (String name : data.keySet()) {
-			this.checkField(name);
-		}
-		this.rowData.putAll(data);
+    /**
+     * Set object field data for writing to the database.
+     * 
+     * @param data
+     *            A Hashmap with property key-value pairs
+     * @throws CorpusDatabaseException
+     *             Thrown if a property does not exist in the database
+     */
+    public void setProperties(HashMap<String, Object> data)
+	    throws CorpusDatabaseException {
+	// check if fields are valid
+	for (String name : data.keySet()) {
+	    this.checkField(name);
 	}
+	this.rowData.putAll(data);
+    }
 
-	public int write() throws CorpusDatabaseException, SQLException {
-		return this.getTable().save(this.rowData);
-	}
+    public int write() throws CorpusDatabaseException, SQLException {
+	return this.getTable().save(this.rowData);
+    }
 }
