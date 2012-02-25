9f4470687f92d1fc66293471eca3334f233c0ad8
penn-narr@web.de <penn-narr@web.de@855ebb53-4e8f-d8f2-0007-f2798c5b8a64>
2012-02-25 20:18:54 +0000

    - updated database scripts - fixed wrong creating of category example data

--- giMMI/database/initialize.sh
+++ giMMI/database/initialize.sh
@@ -3,12 +3,31 @@ HOST=127.0.0.1
 PORT=3306
 USER=root
 
-echo -- Bootstrapping
+echo "=======================[WARNING]======================="
+echo "This will ERASE ALL DATA in the database named 'gimmi'!"
+echo "=======================[WARNING]======================="
+echo "Do you really want to proceed? (y/N)"
+read answer
+if test "$answer" != "Y" -a "$answer" != "y";
+	then exit 0;
+fi
+
+echo -- Creating database
+cd bootstrap
+mysql gimmi -h $HOST --port=$PORT -u $USER < createdb.sql
+cd ..
+
+echo -- Bootstrapping content
 cd bootstrap
 mysql gimmi -h $HOST --port=$PORT -u $USER < bootstrap.sql
 cd ..
 
-echo -- Example Data
+echo "Add example data (for testing)? (y/N)"
+read answer
+if test "$answer" != "Y" -a "$answer" != "y";
+	then exit 0;
+fi
+echo -- Adding example data
 cd exampledata 
 mysql gimmi -h $HOST --port=$PORT -u $USER < exampledata.sql
 cd ..
